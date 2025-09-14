94a36d0217ef724e51a4da29b0801754feff0d24
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2025-09-14 21:09:30 +0900

    :tada:First commit

---
+++ src/main/java/com/ultimate/the_anomaly_record/TheAnomalyRecordApplication.java
@@ -0,0 +1,13 @@
+package com.ultimate.the_anomaly_record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheAnomalyRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheAnomalyRecordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ultimate/the_anomaly_record/TheAnomalyRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultimate.the_anomaly_record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheAnomalyRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
