6b23f39e211feeb67ccfdf60783aae7c2c038703
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2025-09-15 14:34:22 +0900

    :recycle:Refactor Repository 제네릭 타입 명시 (Entity, ID) -> JpaRepository 제네릭 타입 누락으로 인한 테스트 실패 수정

--- src/main/java/com/ultimate/the_anomaly_record/domain/user/dto/UserRequest.java
+++ src/main/java/com/ultimate/the_anomaly_record/domain/user/dto/UserRequest.java
@@ -1,4 +1,12 @@
 package com.ultimate.the_anomaly_record.domain.user.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
 public class UserRequest {
+
+
 }
--- src/test/java/com/ultimate/the_anomaly_record/TheAnomalyRecordApplicationTests.java
+++ src/test/java/com/ultimate/the_anomaly_record/TheAnomalyRecordApplicationTests.java
@@ -2,8 +2,10 @@ package com.ultimate.the_anomaly_record;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TheAnomalyRecordApplicationTests {
 
     @Test
