0939d27bce7dd8b446ad0b92cb0e7def0328a8cf
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2025-09-15 14:40:26 +0900

    :art:Docs: Swagger 구조 추가

---
+++ src/main/java/com/ultimate/the_anomaly_record/global/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.ultimate.the_anomaly_record.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(){
+        Info info = new Info()
+                .version("v1.0") //버전
+                .title("User API")
+                .description("TheAnomalyRecord 유니티 내장서버");
+        return new OpenAPI()
+                .info(info);
+    }
+
+}
