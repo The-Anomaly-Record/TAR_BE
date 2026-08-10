296bcf180f0de15d1dfd9185a6ca54744009976b
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2026-08-10 15:31:37 +0900

    :recycle: Refactor 불필요한 도메인주소 삭제

--- src/main/java/com/ultimate/the_anomaly_record/global/config/SecurityConfig.java
+++ src/main/java/com/ultimate/the_anomaly_record/global/config/SecurityConfig.java
@@ -56,8 +56,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "https://topmapi.duckdns.org"
+                "http://localhost:5173"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
